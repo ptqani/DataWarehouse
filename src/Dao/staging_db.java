27f6f6632419b7cb87e73b07ba459ac43d8f7296
kien <nhukien24112003@gmail.com>
2024-12-02 18:27:29 +0700

    Thay đổi local

--- src/Dao/staging_db.java
+++ src/Dao/staging_db.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class staging_db {
+public class staging_db1 {
     private static final String URL = "jdbc:mysql://localhost:3306/staging_db";
     private static final String USER = "root";
     private static final String PASSWORD = "123456";
